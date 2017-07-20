72f02b9c6acba0b1d124da94fb38ef354bae1151
Cameron Fabbri <cameronfabbri@gmail.com>
2017-07-20 11:07:12 -0500

    seems to be working a little bit

--- architecture.py
+++ architecture.py
@@ -1,44 +1,74 @@
 import tensorflow as tf
+import tensorflow.contrib.layers as layers
+import sys
 
-from tf_ops import *
+'''
+   Leaky RELU
+   https://arxiv.org/pdf/1502.01852.pdf
+'''
+def lrelu(x, leak=0.2, name='lrelu'):
+   return tf.maximum(leak*x, x)
 
-
-def netG(z, BATCH_SIZE):
-
-   z = tf.layers.dense(z, 4*4*1024, name='g_z')
-   z = tf.reshape(z, [BATCH_SIZE, 4, 4, 1024])
-   z = tf.nn.relu(bn(z))
-
-   conv1 = tf.layers.conv2d_transpose(z, 512, 4, strides=2, padding='SAME', name='g_conv1')
-   conv1 = tf.nn.relu(bn(conv1))
+def netG(z, batch_size):
+   print 'GENERATOR'
+   z = layers.fully_connected(z, 4*4*1024, normalizer_fn=layers.batch_norm, activation_fn=tf.nn.relu, scope='g_z')
+   z = tf.reshape(z, [batch_size, 4, 4, 1024])
    
-   conv2 = tf.layers.conv2d_transpose(conv1, 256, 4, strides=2, padding='SAME', name='g_conv2')
-   conv2 = tf.nn.relu(bn(conv2))
-
-   conv3 = tf.layers.conv2d_transpose(conv2, 128, 4, strides=2, padding='SAME', name='g_conv3')
-   conv3 = tf.nn.relu(bn(conv3))
+   conv1 = layers.convolution2d_transpose(z, 512, 5, stride=2, normalizer_fn=layers.batch_norm, activation_fn=tf.nn.relu, scope='g_conv1')
+   conv2 = layers.convolution2d_transpose(conv1, 256, 5, stride=2, normalizer_fn=layers.batch_norm, activation_fn=tf.nn.relu, scope='g_conv2')
+   conv3 = layers.convolution2d_transpose(conv2, 128, 5, stride=2, normalizer_fn=layers.batch_norm, activation_fn=tf.nn.relu, scope='g_conv3')
+   conv4 = layers.convolution2d_transpose(conv3, 3, 5, stride=2, activation_fn=tf.nn.tanh, scope='g_conv4')
    
-   conv4 = tf.layers.conv2d_transpose(conv3, 3, 4, strides=2, padding='SAME', name='g_conv4')
-   conv4 = tf.nn.tanh(conv4)
+   print 'z:',z
+   print 'conv1:',conv1
+   print 'conv2:',conv2
+   print 'conv3:',conv3
+   print 'conv4:',conv4
+   print
+   print 'END G'
+   print
+   tf.add_to_collection('vars', z)
+   tf.add_to_collection('vars', conv1)
+   tf.add_to_collection('vars', conv2)
+   tf.add_to_collection('vars', conv3)
+   tf.add_to_collection('vars', conv4)
 
-   return conv4
+   return conv4 
 
-def netD(images, BATCH_SIZE, reuse=False):
 
-   conv1 = tf.layers.conv2d(images, 64, 4, strides=2, padding='SAME', name='d_conv1', reuse=reuse)
-   conv1 = lrelu(conv1)
+'''
+   Discriminator network
+'''
+def netD(input_images, batch_size, reuse=False):
+   print 'DISCRIMINATOR reuse = '+str(reuse)
+   sc = tf.get_variable_scope()
+   with tf.variable_scope(sc, reuse=reuse):
+      conv1 = layers.conv2d(input_images, 64, 5, stride=2, activation_fn=None, scope='d_conv1')
+      conv1 = lrelu(conv1)
 
-   conv2 = tf.layers.conv2d(conv1, 128, 4, strides=2, padding='SAME', name='d_conv2', reuse=reuse)
-   conv2 = lrelu(conv2)
+      conv2 = layers.conv2d(conv1, 128, 5, stride=2, normalizer_fn=layers.batch_norm, activation_fn=None, scope='d_conv2')
+      conv2 = lrelu(conv2)
 
-   conv3 = tf.layers.conv2d(conv2, 256, 4, strides=2, padding='SAME', name='d_conv3', reuse=reuse)
-   conv3 = lrelu(conv3)
-   
-   conv4 = tf.layers.conv2d(conv3, 512, 4, strides=2, padding='SAME', name='d_conv4', reuse=reuse)
-   conv4 = lrelu(conv4)
+      conv3 = layers.conv2d(conv2, 256, 5, stride=2, normalizer_fn=layers.batch_norm, activation_fn=None, scope='d_conv3')
+      conv3 = lrelu(conv3)
 
-   flat = tf.reshape(conv4, [BATCH_SIZE, -1])
+      conv4 = layers.conv2d(conv3, 512, 5, stride=2, normalizer_fn=layers.batch_norm, activation_fn=None, scope='d_conv4')
+      conv4 = lrelu(conv4)
 
-   dense = lrelu(tf.layers.dense(flat, 1, name='d_dense', reuse=reuse))
+      conv5 = layers.conv2d(conv4, 1, 4, stride=1, activation_fn=None, scope='d_conv5')
+      #conv5 = lrelu(conv5)
 
-   return dense
+      print 'input images:',input_images
+      print 'conv1:',conv1
+      print 'conv2:',conv2
+      print 'conv3:',conv3
+      print 'conv4:',conv4
+      print 'conv5:',conv5
+      print 'END D\n'
+      tf.add_to_collection('vars', conv1)
+      tf.add_to_collection('vars', conv2)
+      tf.add_to_collection('vars', conv3)
+      tf.add_to_collection('vars', conv4)
+      tf.add_to_collection('vars', conv5)
+      return conv5
+
--- train.py
+++ train.py
@@ -1,72 +1,58 @@
-import cPickle as pickle
+import scipy.misc as misc
+import time
 import tensorflow as tf
-from scipy import misc
+from architecture import netD, netG
 import numpy as np
-import argparse
+import random
 import ntpath
 import sys
+import cv2
 import os
-import time
-import imageio
-
-sys.path.insert(0, 'ops/')
-sys.path.insert(0, 'nets/')
-
-from tf_ops import *
+from skimage import color
+import argparse
 import data_ops
-from architecture import *
 
 if __name__ == '__main__':
+
    parser = argparse.ArgumentParser()
-   parser.add_argument('--ARCHITECTURE', required=False,default='dcgan',type=str,help='Architecture to use')
-   parser.add_argument('--DATASET',      required=False,default='celeba',help='The dataset to use')
-   parser.add_argument('--LEARNING_RATE',required=False,default=0.0001,type=float,help='Learning rate for the pretrained network')
-   parser.add_argument('--BATCH_SIZE',   required=False,default=64,type=int,help='Batch size to use')
-   parser.add_argument('--SELU',         required=False,default=0,type=int,help='Whether or not to use SELU')
+   parser.add_argument('--DATASET',    required=True,help='The DATASET to use')
+   parser.add_argument('--DATA_DIR',   required=True,help='Directory where data is')
+   parser.add_argument('--BATCH_SIZE', required=True,help='Batch size',type=int)
    a = parser.parse_args()
 
-   ARCHITECTURE  = a.ARCHITECTURE
-   DATASET       = a.DATASET
-   LEARNING_RATE = a.LEARNING_RATE
-   BATCH_SIZE    = a.BATCH_SIZE
+   DATASET        = a.DATASET
+   DATA_DIR       = a.DATA_DIR
+   BATCH_SIZE     = a.BATCH_SIZE
+   CHECKPOINT_DIR = 'checkpoints/'+DATASET+'/'
+   IMAGES_DIR     = CHECKPOINT_DIR+'images/'
 
-   EXPERIMENT_DIR = 'checkpoints/ARCHITECTURE_'+ARCHITECTURE+'/DATASET_'+DATASET+'/'
-   
-   IMAGES_DIR = EXPERIMENT_DIR+'images/'
-
-   DATA_DIR='/mnt/data2/images/celeba/images/'
-
-   print
-   print 'Creating',EXPERIMENT_DIR
-   try: os.makedirs(IMAGES_DIR)
+   try: os.mkdir('checkpoints/')
+   except: pass
+   try: os.mkdir(CHECKPOINT_DIR)
+   except: pass
+   try: os.mkdir(IMAGES_DIR)
    except: pass
    
-   # global step that is saved with a model to keep track of how many steps/epochs
+   # placeholders for data going into the network
    global_step = tf.Variable(0, name='global_step', trainable=False)
-
-   # placeholder for z
-   z = tf.placeholder(tf.float32, shape=(BATCH_SIZE, 100), name='z')
+   z           = tf.placeholder(tf.float32, shape=(BATCH_SIZE, 100), name='z')
 
    train_images_list = data_ops.loadData(DATA_DIR, DATASET)
-
-   real_images = tf.placeholder(tf.float32, shape=(BATCH_SIZE, 64, 64, 3), name='real_images')
    filename_queue    = tf.train.string_input_producer(train_images_list)
    real_images       = data_ops.read_input_queue(filename_queue, BATCH_SIZE)
 
-   # generate images given z
+   # generated images
    gen_images = netG(z, BATCH_SIZE)
-   
-   # D's decision on real data
-   D_real = netD(real_images, BATCH_SIZE)
 
-   # D's decision on generated data
-   D_fake = netD(gen_images, BATCH_SIZE, reuse=True)
+   # get the output from D on the real and fake data
+   errD_real = netD(real_images, BATCH_SIZE)
+   errD_fake = netD(gen_images, BATCH_SIZE, reuse=True)
 
-   errD = tf.reduce_mean(D_fake)-tf.reduce_mean(D_real)
-   errG = -tf.reduce_mean(D_fake)
+   # cost functions
+   errD = tf.reduce_mean(errD_real) - tf.reduce_mean(errD_fake)
+   errG = tf.reduce_mean(errD_fake)
    
-   # gradient penalty stuff
-   epsilon = tf.random_uniform([BATCH_SIZE, 1,1,1], 0.0, 1.0)
+   epsilon = tf.random_uniform([], 0.0, 1.0)
    x_hat = real_images*epsilon + (1-epsilon)*gen_images
    d_hat = netD(x_hat, BATCH_SIZE, reuse=True)
    gradients = tf.gradients(d_hat, x_hat)[0]
@@ -75,33 +61,33 @@ if __name__ == '__main__':
    errD += gradient_penalty
 
    # tensorboard summaries
-   try: tf.summary.scalar('d_loss', tf.reduce_mean(errD))
-   except:pass
-   try: tf.summary.scalar('g_loss', tf.reduce_mean(errG))
-   except:pass
+   tf.summary.scalar('d_loss', errD)
+   tf.summary.scalar('g_loss', errG)
+   merged_summary_op = tf.summary.merge_all()
 
    # get all trainable variables, and split by network G and network D
    t_vars = tf.trainable_variables()
    d_vars = [var for var in t_vars if 'd_' in var.name]
    g_vars = [var for var in t_vars if 'g_' in var.name]
 
-   G_train_op = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE,beta1=0.5,beta2=0.9).minimize(errG, var_list=g_vars, global_step=global_step)
-   D_train_op = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE,beta1=0.5,beta2=0.9).minimize(errD, var_list=d_vars)
+   # optimize G
+   G_train_op = tf.train.AdamOptimizer(learning_rate=1e-4,beta1=0.0,beta2=0.9).minimize(errG, var_list=g_vars, global_step=global_step)
+
+   # optimize D
+   D_train_op = tf.train.AdamOptimizer(learning_rate=1e-4,beta1=0.0,beta2=0.9).minimize(errD, var_list=d_vars)
 
    saver = tf.train.Saver(max_to_keep=1)
-   
    init = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
-   sess = tf.Session()
+   sess  = tf.Session()
    sess.run(init)
 
-   # write out logs for tensorboard to the checkpointSdir
-   summary_writer = tf.summary.FileWriter(EXPERIMENT_DIR+'/logs/', graph=tf.get_default_graph())
+   summary_writer = tf.summary.FileWriter(CHECKPOINT_DIR+'/'+'logs/', graph=tf.get_default_graph())
 
-   tf.add_to_collection('vars', G_train_op)
-   tf.add_to_collection('vars', D_train_op)
-
-   ckpt = tf.train.get_checkpoint_state(EXPERIMENT_DIR)
+   tf.add_to_collection('G_train_op', G_train_op)
+   tf.add_to_collection('D_train_op', D_train_op)
+   
    # restore previous model if there is one
+   ckpt = tf.train.get_checkpoint_state(CHECKPOINT_DIR)
    if ckpt and ckpt.model_checkpoint_path:
       print "Restoring previous model..."
       try:
@@ -112,37 +98,43 @@ if __name__ == '__main__':
          pass
    
    ########################################### training portion
-   step = sess.run(global_step)
-   merged_summary_op = tf.summary.merge_all()
 
+   step = sess.run(global_step)
+   
    coord = tf.train.Coordinator()
    threads = tf.train.start_queue_runners(sess, coord=coord)
 
+   n_critic = 5
+
    while True:
+      
+      start = time.time()
 
+      # train the discriminator for 5 or 25 runs
+      for critic_itr in range(n_critic):
          batch_z = np.random.normal(-1.0, 1.0, size=[BATCH_SIZE, 100]).astype(np.float32)
-         sess.run([G_train_op], feed_dict={z:batch_z})
+         sess.run(D_train_op, feed_dict={z:batch_z})
 
-         for itr in xrange(5):
-            batch_z = np.random.normal(-1.0, 1.0, size=[BATCH_SIZE, 100]).astype(np.float32)
-            sess.run([D_train_op], feed_dict={z:batch_z})
+      # now train the generator once! use normal distribution, not uniform!!
+      batch_z = np.random.normal(-1.0, 1.0, size=[BATCH_SIZE, 100]).astype(np.float32)
+      sess.run(G_train_op, feed_dict={z:batch_z})
 
-         batch_z = np.random.normal(-1.0, 1.0, size=[BATCH_SIZE, 100]).astype(np.float32)
-         D_loss, G_loss, summary = sess.run([errD, errG, merged_summary_op], feed_dict={z:batch_z})
+      # now get all losses and summary *without* performing a training step - for tensorboard
+      D_loss, G_loss, summary = sess.run([errD, errG, merged_summary_op], feed_dict={z:batch_z})
+      summary_writer.add_summary(summary, step)
 
-         summary_writer.add_summary(summary, step)
-         print 'step:',step,'D loss:',D_loss,'G_loss:',G_loss
-         step += 1
-         
-         if step%100 == 0:
-            print 'Saving model...'
-            saver.save(sess, EXPERIMENT_DIR+'checkpoint-'+str(step))
-            saver.export_meta_graph(EXPERIMENT_DIR+'checkpoint-'+str(step)+'.meta')
-            print 'Model saved\n'
+      print 'step:',step,'D loss:',D_loss,'G_loss:',G_loss,'time:',time.time()-start
+      step += 1
+    
+      if step%100 == 0:
+         print 'Saving model...'
+         saver.save(sess, CHECKPOINT_DIR+'checkpoint-'+str(step))
+         saver.export_meta_graph(CHECKPOINT_DIR+'checkpoint-'+str(step)+'.meta')
+         batch_z  = np.random.normal(-1.0, 1.0, size=[BATCH_SIZE, 100]).astype(np.float32)
+         gen_imgs = sess.run([gen_images], feed_dict={z:batch_z})
 
-            batch_z  = np.random.normal(-1.0, 1.0, size=[BATCH_SIZE, 100]).astype(np.float32)
-            gen_imgs = sess.run([gen_images], feed_dict={z:batch_z})
+         data_ops.saveImage(gen_imgs[0], step, IMAGES_DIR)
+         print 'Done saving'
 
-            data_ops.saveImage(gen_imgs[0], step, IMAGES_DIR)
-            print 'Done saving'
+
 
